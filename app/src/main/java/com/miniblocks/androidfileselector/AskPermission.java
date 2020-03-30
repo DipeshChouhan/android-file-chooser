0761b3f534daef240941ee03723507c77cffdc37
Dipesh Chouhan <dcdon077@gmail.com>
2020-03-30 21:52:52 +0530

    AskPermission helper class added

--- app/src/main/java/com/miniblocks/androidfileselector/AskPermission.java
+++ app/src/main/java/com/miniblocks/androidfileselector/AskPermission.java
@@ -1,4 +1,36 @@
 package com.miniblocks.androidfileselector;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AskPermission {
+
+    public static final int PERMISSION_CODE = 7020;
+    private Activity mActivity;
+    private ArrayList<String> permissionsToRequest = new ArrayList<>();
+    public AskPermission(Activity context, String ...permissions){
+        mActivity = context;
+        for(String perm: permissions){
+            if(ContextCompat.checkSelfPermission(context, perm) !=
+                    PackageManager.PERMISSION_GRANTED){
+                permissionsToRequest.add(perm);
+            }
+        }
+    }
+
+    public void request(){
+        String[] permissions = Arrays.copyOf(permissionsToRequest.toArray(),
+                permissionsToRequest.size(), String[].class);
+        ActivityCompat.requestPermissions(mActivity, permissions, PERMISSION_CODE);
+    }
+
 }
--- app/src/main/java/com/miniblocks/androidfileselector/MainActivity.java
+++ app/src/main/java/com/miniblocks/androidfileselector/MainActivity.java
@@ -18,14 +18,9 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinkedList<String> permissions = new LinkedList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
-            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-        }
-
 
 
         setContentView(R.layout.activity_main);
@@ -33,22 +28,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void requestButton(View view){
-        for(String permission : permissions){
-            if(ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
-                permissions.remove(permission);
-            }
-        }
-        requestPermissions();
-    }
-
-    public void requestPermissions(){
-        String[] array = new String[permissions.size()];
-        for(int i = 0; i < permissions.size(); i++){
-            array[i] = permissions.get(i);
-        }
-        if(permissions.size() != 0){
-            ActivityCompat.requestPermissions(this, array, 2);
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN){
+            AskPermission permissions = new AskPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            permissions.request();
         }
 
     }
+
+
 }
