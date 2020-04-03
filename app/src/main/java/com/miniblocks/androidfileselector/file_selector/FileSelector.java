a7f30ab186057b1c9840809079c4e903afd5737d
Dipesh Chouhan <dcdon077@gmail.com>
2020-04-03 13:09:01 +0530

    cleaning up FileSelector.java

--- app/src/main/java/com/miniblocks/androidfileselector/file_selector/FileSelector.java
+++ app/src/main/java/com/miniblocks/androidfileselector/file_selector/FileSelector.java
@@ -1,20 +1,8 @@
 package com.miniblocks.androidfileselector.file_selector;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.miniblocks.androidfileselector.R;
 import com.miniblocks.androidfileselector.file_selector.util.FileUtil;
 
 import java.io.File;
@@ -418,66 +406,6 @@ public class FileSelector extends SelectorCallbacks {
         return null;
     }
 
-//    public static class FileView1 extends Fragment{
-//        private SelectorCallbacks mCallbacks;
-//        private String toolbarTitleText;
-//        private String toolbarSubTitleText;
-//
-//        @Nullable
-//        @Override
-//        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//            View view = inflater.inflate(R.layout.recycler_view, container, false);
-//            RecyclerView recyclerView = view.findViewById(R.id.recycler_view_id);
-//            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//            RecyclerViewAdapter adapter = new RecyclerViewAdapter(mCallbacks,
-//                    listOfSimpleFiles);
-//            recyclerView.setAdapter(adapter);
-//            Toolbar toolbar = view.findViewById(R.id.toolbar_id);
-//
-//            ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-//            ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
-//            ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//            toolbar.setTitle(toolbarTitleText);
-//            if(toolbarSubTitleText != null) toolbar.setSubtitle(toolbarSubTitleText);
-//            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    onBackButtonClicked();
-//                }
-//            });
-//            return view;
-//        }
-//
-//        public void config(SelectorCallbacks callbacks){
-//            mCallbacks = callbacks;
-//        }
-//
-//        private void onBackButtonClicked(){
-//            getFragmentManager().beginTransaction().remove(this).commit();
-//        }
-//
-//        /**
-//         *
-//         * @param titleText - for toolbar title.
-//         */
-//        public void setToolbarTitleText(String titleText){
-//            toolbarTitleText = titleText;
-//        }
-//
-//        /**
-//         *
-//         * @param subTitleText - for toolbar subtitle.
-//         */
-//        public void setToolbarSubTitleText(String subTitleText){
-//
-//            toolbarSubTitleText = subTitleText;
-//
-//        }
-
-
-
-//
-//    }
 
     public void onResumedFragment(FragmentManager fragmentManager){
         mFragmentManager = fragmentManager;
